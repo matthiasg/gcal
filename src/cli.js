7cb3fd8e23d5cf01023c6261bc1c9b29a123ebb8
Matthias Goetzke <m.goetzke@curasystems.de>
2021-12-12 13:41:59 +0100

    added configuration and runtime options caps

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,21 @@
-console.log('TEST')
+import fs from 'fs'
+import yargs from 'yargs'
+
+const args = yargs(process.argv.slice(2))
+  .config('config', function (configPath) {
+    return JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+  })
+  .option('clientId', {
+    description: 'Google Client ID to access Google Calendar',
+    demandOption: true,
+  })
+  .option('clientSecret', {
+    description: 'Google Client Secret to access Google Calendar',
+    demandOption: true,
+  })
+  .argv
+
+console.log('TEST', args)
+
+
+// import { CLIENT_ID, CLIENT_SECRET } from '/etc't
